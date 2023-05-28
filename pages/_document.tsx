7599c734328f40bf69d2aaaf9e55a787a3d13186
yarikLev04 <91241935+yarikLev04@users.noreply.github.com>
2023-05-28 19:49:51 +0300

    initial project with some linters

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { JSX } from 'react';
 
-export default function Document() {
+export default function Document(): JSX.Element {
   return (
     <Html lang="en">
       <Head />
@@ -9,5 +10,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import { JSX } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <>
       <Head>
@@ -110,5 +111,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }

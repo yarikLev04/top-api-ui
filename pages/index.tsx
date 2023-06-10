6087d6e91de07e92188e7d493da8d3ac84fd9249
yarikLev04 <91241935+yarikLev04@users.noreply.github.com>
2023-06-10 22:02:46 +0300

    Add some part of routing and static generation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import { JSX, useState } from "react";
 import { Button, Htag, Paragraph, Rating, Tag } from "@/components";
 import { withLayout } from "@/layout/Layout";
+import { GetStaticProps } from "next";
+import axios from "axios";
+import { MenuItem } from "@/interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(2);
 
   return (
@@ -35,8 +38,36 @@ function Home(): JSX.Element {
       </Tag>
       <Tag size="s">m</Tag>
       <Rating isEditable={true} rating={rating} setRating={setRating}></Rating>
+      <ul>
+        {menu.map((menuItem) => (
+          <li key={menuItem._id.secondCategory}>
+            {menuItem._id.secondCategory}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+
+  const { data: menu } = await axios.post<MenuItem[]>(
+    (process.env.NEXT_PUBLIC_DOMAIN as string) + "/api/top-page/find",
+    { firstCategory }
+  );
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+export interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
